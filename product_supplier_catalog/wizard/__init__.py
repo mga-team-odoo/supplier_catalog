91632446a7dde22597740aa7efe4b50764e8ef21
Christophe CHAUVET <christophe.chauvet@gmail.com>
2016-11-13 17:23:37 +0100

    [FIX] Fix relative import

--- product_supplier_catalog/wizard/__init__.py
+++ product_supplier_catalog/wizard/__init__.py
@@ -22,7 +22,6 @@
 #
 ##############################################################################
 
-import create_product
-
+from . import create_product
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
